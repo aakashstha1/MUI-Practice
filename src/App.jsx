602a25ee1b1d4168ea979fd56e82d55b02074ece
aakashstha1 <aakash.cerestha90@gmail.com>
2024-06-12 14:07:42 +0545

    Day12

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+// import { MuiAvatar } from "./Day13/MuiAvatar";
 import "./App.css";
+// import { MuiBadge } from "./Day13/MuiBadge";
+// import { MuiList } from "./Day14/MuiList";
+import { MuiToolTip } from "./Day14/MuiToolTip";
+// import MuiBtnNav from "./Day12/MuiBtnNav";
+// import MuiSpeedDial from "./Day12/MuiSpeedDial";
 // import { MuiBreadcrumbs } from "./Day11/MuiBreadcrumbs";
-import  MuiDrawer  from "./Day11/MuiDrawer";
+// import  MuiDrawer  from "./Day11/MuiDrawer";
 
 function App() {
   return (
     <>
       {/* <MuiBreadcrumbs /> */}
-      <MuiDrawer />
+      {/* <MuiDrawer /> */}
+      {/* <MuiSpeedDial /> */}
+      {/* <MuiBtnNav /> */}
+      {/* <MuiAvatar /> */}
+      {/* <MuiBadge /> */}
+      {/* <MuiList /> */}
+      <MuiToolTip />
     </>
   );
 }
